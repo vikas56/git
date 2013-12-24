720924ccbb0f230305056bd840d111147d9b9741
vikas56 <vikas00959@gmail.com>
2013-12-24 11:19:48 +0530

    Create sample1.php

---
+++ sample1.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "login";
+//include("sample.php");
+?>
+<html>
+<body>
+<input type="textbox" name="txt1"/><br>
+<input type="textbox" name="txt2"/><br>
+<input type="submit" name="button"/><br>
+</form>
+</body>
+</html>
